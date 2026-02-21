42158370fe2c0cf15a86408e384473a75af4ebf8
Aadit Garg <69139031+Aadit-Garg@users.noreply.github.com>
2026-02-21 16:38:53 +0530

    Convert website to Progressive Web App (PWA) allowing mobile Home Screen installation

---
+++ static/sw.js
@@ -0,0 +1,30 @@
+// Simple Service Worker for PWA
+const CACHE_NAME = 'iic-club-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/style.css',
+  '/static/js/main.js',
+  '/static/iiclogo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
